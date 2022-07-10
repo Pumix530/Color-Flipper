ad9548c11c612adaf0f201b9a112b245861fbe9f
Pumix530 <105774973+Pumix530@users.noreply.github.com>
2022-07-10 14:15:16 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const colors = ["#000080", "#00008B", "#0000CD", "#0000FF","#006400", "#008000", "#008080",]
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click",  () => {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
